eae703a694688a981e0902b56a147a6555ef00b7
Bobshop verbytski <nikolasdelunko@gmail.com>
2023-03-14 17:52:17 +0200

    question was adapted

--- src/Components/Question/Window.js
+++ src/Components/Question/Window.js
@@ -5,8 +5,8 @@ import PlusIco from "../Theme/Icons/PlusIco";
 export default function Window({ name, text }) {
   const [open, setOpen] = useState(false);
   return (
-    <div class="flex max-w-[643px] rounded-[20px]  bg-regular px-[24px] py-[35px] mx-[24px] my-[20px] ">
-      <div class="flex flex-col">
+    <div class="flex max-w-[643px] rounded-[20px]  bg-regular px-[24px] py-[35px] mx-[24px] my-[20px] relative">
+      <div class="flex flex-col mr-[100px]">
         <h2 class="font-mainText text-[20px] text-main-color">{name}</h2>
         {open && (
           <p class="font-textSec text-[18px] max-w-[380px] text-white">
@@ -15,7 +15,7 @@ export default function Window({ name, text }) {
         )}
       </div>
       <div
-        class="cursor-pointer ml-[109px]"
+        class="cursor-pointer  absolute right-[34px]"
         onClick={() => {
           setOpen(!open);
         }}
