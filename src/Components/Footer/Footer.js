16ea0b667273e80029c95e4c01cc9b8bab5ef6f8
Bobshop verbytski <nikolasdelunko@gmail.com>
2023-03-14 19:11:00 +0200

    footer fix done

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div class="flex items-center justify-between px-[60px] py-[35px]  bg-main-color w-[100%] absolute bottom-0 max-[420px]:flex-col">
+    <div class="flex items-center justify-between px-[60px] py-[35px]  bg-main-color w-[100%] absolute bottom-0 max-[420px]:flex-col max-[420px]:justify-center">
       <div class="flex items-center">
         <img src={require("../Theme/Img/logo.png")} alt="Logo" />
       </div>
