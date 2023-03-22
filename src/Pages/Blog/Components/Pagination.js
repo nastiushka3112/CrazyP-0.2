d6f6f1c064fea8d6c4cc8caf42ad446adf6930e1
Bobshop verbytski <nikolasdelunko@gmail.com>
2023-03-22 19:39:32 +0200

    pagination done

--- src/Pages/Blog/Components/Pagination.js
+++ src/Pages/Blog/Components/Pagination.js
@@ -1,29 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { setPagination } from "../../../store/helpers/helpersSlice";
 import { useDispatch, useSelector } from "react-redux";
 import Next from "../../../Components/Theme/Icons/Next";
 
 export default function Pagination({ elements }) {
   const [fromPage, setFromPage] = useState(0);
+  const [toPage, setToPage] = useState(4);
   const dispatch = useDispatch();
   const activeP = useSelector((state) => state.helpers.pagination);
 
   const Arr = [];
-	
+
   const summPages = () => {
     const a = elements / 12;
     return Math.ceil(a);
   };
 
-	if(summPages() > 4) {
-		setFromPage(fromPage + 1)
-	}
+  useEffect(() => {
+    if (activeP === toPage && activeP !== summPages()) {
+      setToPage(toPage + 3);
+      setFromPage(fromPage + 3);
+    } else if (activeP === fromPage && activeP !== 1) {
+      setToPage(toPage - 3);
+      setFromPage(fromPage - 3);
+    }
+  }, [activeP]);
 
-	for (let i = 1; i <= summPages(); i++){
-		Arr.push(i)
-	}
+  for (let i = 1; i <= summPages(); i++) {
+    Arr.push(i);
+  }
 
-  console.log(summPages(), elements);
+  console.log(summPages(), toPage, toPage - 1);
 
   const Numbers = (num) => {
     return (
@@ -32,7 +39,7 @@ export default function Pagination({ elements }) {
           dispatch(setPagination(num));
         }}
       >
-        {activeP == num ? (
+        {activeP === num ? (
           <div class="bg-main-color w-[64px] h-[64px] flex justify-center items-center rounded-[50%] cursor-pointer">
             <p class="text-mainText text-regular text-[32px]">{num}</p>
           </div>
@@ -47,20 +54,42 @@ export default function Pagination({ elements }) {
 
   return (
     <div class="flex gap-[8px] justify-center">
-      {Arr.slice(fromPage, summPages()).map((el) => Numbers(el))}
-      <div class="bg-[#C5C3C3] w-[64px] h-[64px] flex justify-center items-center rounded-[50%] cursor-pointer">
-        <p class="text-mainText text-[#433E3E] text-[32px]">...</p>
-      </div>
-      <div class="bg-[#C5C3C3] w-[64px] h-[64px] flex justify-center items-center rounded-[50%] cursor-pointer">
-        <p
-          class="text-mainText text-[#433E3E] text-[32px]"
+      {activeP !== 1 && (
+        <div
+          style={{ transform: "rotate(180deg)" }}
+          class="bg-[#C5C3C3] w-[64px] h-[64px] flex justify-center items-center rounded-[50%] cursor-pointer"
+          onClick={() => {
+            dispatch(setPagination(activeP - 1));
+          }}
+        >
+          <div class="text-mainText text-[#433E3E] text-[32px]">
+            <Next />
+          </div>
+        </div>
+      )}
+      {Arr.slice(fromPage, toPage).map((el) => Numbers(el))}
+      {toPage !== summPages() && summPages() >= toPage - 1 && (
+        <div
+          onClick={() => {
+            setToPage(toPage + 4);
+          }}
+          class="bg-[#C5C3C3] w-[64px] h-[64px] flex justify-center items-center rounded-[50%] cursor-pointer"
+        >
+          <p class="text-mainText text-[#433E3E] text-[32px]">...</p>
+        </div>
+      )}
+      {activeP !== summPages() && (
+        <div
           onClick={() => {
             dispatch(setPagination(activeP + 1));
           }}
+          class="bg-[#C5C3C3] w-[64px] h-[64px] flex justify-center items-center rounded-[50%] cursor-pointer"
         >
-          <Next />
-        </p>
-      </div>
+          <div class="text-mainText text-[#433E3E] text-[32px]">
+            <Next />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
